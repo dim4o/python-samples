779088a8a0661ef02bd9a6e7066ef6dae1e95124
dimcho <dimcho.nedev+git@gmail.com>
2017-10-19 23:58:50 +0300

    Add maximum non-adjacent subsequence sum algorithm

---
+++ max_sum_subsequence_non_adjacent.py
@@ -0,0 +1,33 @@
+def find_max_non_adjacent_subsequence_sum(sequence):
+    """
+    Given an array of positive number, find maximum sum subsequence such that elements
+    in this subsequence are not adjacent to each other.
+    For better context see: https://youtu.be/UtGtF6nc35g
+
+    Example: [4, 1, 1, 4, 2, 1], max_sum = 4 + 4 + 1 = 9
+    Rule:
+    temp = incl
+    incl = max(incl, excl + sequence[i])
+    excl = temp
+    ...
+    max_subseq = max(incl, excl)
+
+    :param sequence: the given sequence
+    :return: max subsequence sum
+    """
+    incl = 0
+    excl = 0
+
+    for i in range(len(sequence)):
+        temp = incl
+        incl = max(incl, excl + sequence[i])
+        excl = temp
+
+    return max(incl, excl)
+
+
+# test
+print find_max_non_adjacent_subsequence_sum(sequence=[1])  # 1
+print find_max_non_adjacent_subsequence_sum(sequence=[7, 8])  # 8
+print find_max_non_adjacent_subsequence_sum(sequence=[4, 1, 1, 4, 2, 1])  # 9
+print find_max_non_adjacent_subsequence_sum(sequence=[1, 2, 9, 10, 6, 7, 12])  # 28
